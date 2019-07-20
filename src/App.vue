5f02d7c333cfc7c51bd6197c619304f328a59f3f
hamer1563 <hamzah.amer@spathesystems.com>
2019-07-20 16:28:21 -0400

    inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Testing</h1>
+    <sidemenu></sidemenu>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import sidemenu from '@/components/sidemenu.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components:{
+    sidemenu
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/sidemenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="sidemenu">
+    <div></div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'sidemenu',
+}
+</script>
+
+<style>
+</style>
