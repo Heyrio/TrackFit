21a15b3a632b62c3d987cf9ccd52ce15e5cce97f
hamer1563 <hamzah.amer@spathesystems.com>
2019-07-21 13:52:41 -0400

    more improvements to the menu bar

--- src/components/sidemenu.vue
+++ src/components/sidemenu.vue
@@ -1,31 +1,62 @@
 <template>
-
     <div class="side-menu">
-      <div class="name">
-        <div class="photo">
-          <h1>USERNAME</h1>
-          <img src="@/assets/images/me.jpg">
+      <div class="columns">
+        <div class="column">
+          <div class="name">
+            <div class="photo">
+              <h1>Hamzah Amer</h1>
+                <img class="responsive " src="@/assets/images/me.jpg">
+            </div>
+          </div>
         </div>
       </div>
+     <div class="columns">
+        <div class="column">
+          <div>
+        <aside class="menu">
+        <ul class="menu-list">
+          <li>
+            <ul>
+              <a>Stats</a>
+              <a>Graph</a>
+              <a>Lorem, iporibus harum.</a>
+              <a>Invitati;dslfkmasmdfons</a>
+              <a>CloudsfsdfSettings</a>
+              <a>Autdsfasdtion</a>
+            </ul>
+          </li>
+        </ul>
+      </aside>
+     </div>
     </div>
-  
+  </div>
+      
+    </div>
 </template>
 
 <script>
-
-
 export default {
   name: 'sidemenu',
 }
 </script>
 
 <style>
+ a{
+  color: #fff !important;
+  font-weight: 600;
+}
+a:hover{
+  color: #43B984 !important;
+}
+.photo h1{
+  text-align: center;
+  margin-bottom: 25px !important;
+}
 .photo img {
   border-radius: 200px;
   height: 200px;
 }
 .photo{
-
   float: right;
   width: 200px;
   height: 200px;
