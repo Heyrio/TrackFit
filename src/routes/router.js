f37da4d1f6692b3ad86e52411f8e758e5bb2dcea
hamer1563 <hamzah.amer@spathesystems.com>
2019-07-23 13:30:47 -0400

    added a heat map to the dashboard

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import dash from '@/views/dash'
-
+import stats from '@/views/stats'
 Vue.use(Router)
 
 export default new Router({
@@ -10,5 +10,9 @@ export default new Router({
         path: '/dashboard',
         name: 'Dashboard',
         component: dash,
+    }, {
+        path: '/stats',
+        name: 'Stats',
+        component: stats,
     }]
 })
