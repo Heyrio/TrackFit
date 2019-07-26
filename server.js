4ac040b5f1e8a4210dd5c2c333f5364c17807ee0
Hamzah Amer <hamer1563@gmail.com>
2019-07-26 14:02:39 -0400

    data is now showing on client side

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Post = require('./Post');
 const app = express();
 
+let data = null;
+let newData = null;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //Connecting to MongoDB 
-const URI = 'mongodb+srv://admin:passabc@cluster0-zfho8.mongodb.net/test?retryWrites=true&w=majority'
+const URI = 'mongodb+srv://admin:260182abc@cluster0-zfho8.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(URI, { useNewUrlParser: true }).then(() => {
-    /*eslint-disable*/
-    console.log('Database connected...')
-}).catch((err) => {
-    console.log(err);
-})
-
-Post.find({}, (err, posts) => {
-        console.log(posts)
-    }),
-
-    app.use(morgan('tiny'));
+        /*eslint-disable*/
+        console.log('Database connected...')
+    }).catch((err) => {
+        console.log(err);
+    })
+    // Needs to be above the requests
 app.use(cors());
-app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send(data);
+});
+
+
+//CRUD operations
+Post.find({}, (err, posts) => {
+    data = posts;
+});
+
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
     /*eslint-disable*/
-    console.log('server is running');
+    console.log('server is running on port:', port);
 });
