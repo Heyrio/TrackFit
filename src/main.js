3399a83fb591cb99ac9670009ea8c36d385943c1
Hamzah Amer <hamer1563@gmail.com>
2019-07-29 11:52:38 -0400

    adding drop down

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import './../node_modules/bulma/css/bulma.css';
 import './../src/assets/styles/global.css';
 import router from './routes/router';
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
+Vue.use(Buefy)
 Vue.config.productionTip = false
 
 new Vue({
